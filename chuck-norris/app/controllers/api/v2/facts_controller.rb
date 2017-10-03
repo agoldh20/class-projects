f101e334e4ab79535bdcfc50b9b3d7441a9acddd
agoldwater <dmgldwtr@gmail.com>
2017-10-03 09:56:12 -0500

    fixed destroy method render

--- chuck-norris/app/controllers/api/v2/facts_controller.rb
+++ chuck-norris/app/controllers/api/v2/facts_controller.rb
@@ -14,11 +14,10 @@ class Api::V2::FactsController < ApplicationController
 
   def destroy
     @fact = ChuckNorrisFact.find(params[:id]).destroy
-    flash = "You might have Destoyed this one fact...but Chuck Norris just created 10 MORE in its place!"
     10.times do
       ChuckNorrisFact.create(fact: Faker::ChuckNorris.fact)
     end
     @facts = ChuckNorrisFact.all
-    render "/api/v2/facts.json"
+    render json: {message: "You might have Destoyed this one fact...but Chuck Norris just created 10 MORE in its place!"}, code: 200
   end
 end
